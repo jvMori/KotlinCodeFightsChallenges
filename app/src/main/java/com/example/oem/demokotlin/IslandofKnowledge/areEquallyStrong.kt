5d96ac606af9b5d851ffe36e84d3dc17d77bcdf1
Mori <iwona.moryc92@gmail.com>
2019-01-21 13:08:31 +0100

    Update areEquallyStrong.kt

--- app/src/main/java/com/example/oem/demokotlin/IslandofKnowledge/areEquallyStrong.kt
+++ app/src/main/java/com/example/oem/demokotlin/IslandofKnowledge/areEquallyStrong.kt
@@ -2,7 +2,9 @@ package com.example.oem.demokotlin.IslandofKnowledge
 
 class AreEquallyStrong{
     fun areEquallyStrong(yourLeft: Int, yourRight: Int, friendsLeft: Int, friendsRight: Int): Boolean {
+        val you  = listOf(yourLeft, yourRight)
+        val friend = listOf(friendsLeft, friendsRight)
 
-        return false
+        return (you.max() == friend.max() && you.min() == friend.min())
     }
 }
