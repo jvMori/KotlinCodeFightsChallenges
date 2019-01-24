7ca9540e159e15a39d0b6d17381c9143451a49da
Mori <iwona.moryc92@gmail.com>
2019-01-24 15:35:05 +0100

    Create BoxBlur.kt

---
+++ app/src/main/java/com/example/oem/demokotlin/IslandofKnowledge/BoxBlur.kt
@@ -0,0 +1,13 @@
+package com.example.oem.demokotlin.IslandofKnowledge
+
+class BoxBlur
+{
+    fun boxBlur(image: MutableList<MutableList<Int>>): MutableList<MutableList<Int>> =
+        (1 until image.lastIndex).map { x ->
+            (1 until image[x].lastIndex).map { y ->
+                image.slice(x - 1..x + 1)
+                    .flatMap { it.slice(y - 1..y + 1) }
+                    .sum() / 9
+            }.toMutableList()
+        }.toMutableList()
+}
